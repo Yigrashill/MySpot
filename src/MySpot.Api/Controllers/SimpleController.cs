231aa6559b1e75d7bc8bb6bc231528bce3667368
Yigrashill <tomek198734@wp.pl>
2024-05-15 18:02:17 +0200

    Add test Simple Use Controller

---
+++ src/MySpot.Api/Controllers/SimpleController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySpot.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SimpleController : ControllerBase
+    {
+        [HttpGet(Name ="GetSimple")]
+        public IEnumerable<string> GetStrings() 
+        {
+            var names = new List<string>
+            {
+                "Super",
+                "Super2"
+            };
+            return names;
+        }
+    }
+}
